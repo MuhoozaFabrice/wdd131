01c79f5af3f0f70b7da0fff9f472336925299f07
MuhoozaFabrice <muhozafabrice888@gmail.com>
2025-09-06 17:46:40 +0300

    W01

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Dynamically set copyright year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Display last modified date
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
